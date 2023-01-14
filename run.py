4a69f78f4ebcbc817d0eda17116206dfb78daa0d
CheyenneMDavid <Cheyennedavid68@gmail.com>
2023-01-14 01:22:38 +0000

    Add commemted code memory aid for refactoring

--- run.py
+++ run.py
@@ -55,6 +55,31 @@ def validate_data(values):
 
     return True
 
+# ----  Two functions which were refactored into one, so no longer
+# ----  required. But left here in comments for assistance.
+
+# def update_sales_worksheet(data):
+#     """
+#     Update sales worksheet, add new row with the list data provided.
+#     """
+#     print("Updating sales worksheet...\n")
+#     sales_worksheet = SHEET.worksheet("sales")
+#     sales_worksheet.append_row(data)
+#     print("Sales worksheet updated successfully.\n")
+
+
+# def update_surplus_worksheet(data):
+#     """
+#     Update surplus worksheet, add new row with the list data provided
+#     """
+#     print("Updating surplus worksheet...\n")
+#     surplus_worksheet = SHEET.worksheet("surplus")
+#     surplus_worksheet.append_row(data)
+#     print("Surplus worksheet updated successfully.\n")
+
+# ----  Two functions which were refactored into one, so no longer
+# ----  required. But left here in comments for assistance.
+
 
 def update_worksheet(data, worksheet):
     """
